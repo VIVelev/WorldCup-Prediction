44464b8740515d68be67239d1d3f8018f973c6b2
kristyanYochev <kris.yochev@gmail.com>
2018-06-10 23:21:11 +0300

    fixed setup_file.py

--- site/server/setup_file.py
+++ site/server/setup_file.py
@@ -14,7 +14,7 @@ for match in get_next_day_matches():
     match.prob_away = probs[2]
     match.prob_draw = probs[0]
 
-    matches.append(match)
+    matches.append(match.__dict__)
 
 with open("matches.json", "w") as matches_file:
     matches_file.write(json.dumps(matches, indent=4))
