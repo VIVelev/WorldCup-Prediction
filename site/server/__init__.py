d69539d8e03065cd8c87b82313eb2fba4f2c76be
Victor Velev <velev.victor@yahoo.com>
2018-06-10 23:20:58 +0300

    get 11 best players

--- site/server/__init__.py
+++ site/server/__init__.py
@@ -3,12 +3,12 @@ import routes
 from match_predictor.main import predict_proba
 
 if __name__ == "__main__":
-    print("-"*30)
-    print(
-        predict_proba(
-            "Group A", 4000,
-            "Brazil", "Germany"
-        )
-    )
-    print("-"*30)
+    # print("-"*30)
+    # print(
+    #     predict_proba(
+    #         "Group A", 4000,
+    #         "Brazil", "Germany"
+    #     )
+    # )
+    # print("-"*30)
     app.run(debug=True)
