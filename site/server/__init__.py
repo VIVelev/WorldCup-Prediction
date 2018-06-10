5ba6285df9cf53ef637b7664040d01ac4da519ca
kristyanYochev <kris.yochev@gmail.com>
2018-06-10 23:48:44 +0300

    Fixed datetime issue

--- site/server/__init__.py
+++ site/server/__init__.py
@@ -1,6 +1,6 @@
 from app import *
 import routes
-from match_predictor.main import predict_proba
+# from match_predictor.main import predict_proba
 
 if __name__ == "__main__":
     # print("-"*30)
--- site/server/setup_file.py
+++ site/server/setup_file.py
@@ -1,20 +1,20 @@
 import json
-from match_predictor.main import predict_proba
+# from match_predictor.main import predict_proba
 from scraping import get_next_day_matches
 
 matches = []
 
 for match in get_next_day_matches():
-    probs = predict_proba(
-        match.stage, 4000,
-        match.home, match.away
-    )
-
+    # probs = predict_proba(
+    #     match.stage, 4000,
+    #     match.home, match.away
+    # )
+    probs = 10, 60, 30
     match.prob_home = probs[1]
     match.prob_away = probs[2]
     match.prob_draw = probs[0]
 
-    matches.append(match.__dict__)
+    matches.append(dict(match))
 
 with open("matches.json", "w") as matches_file:
     matches_file.write(json.dumps(matches, indent=4))
