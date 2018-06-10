ebce7cc1cfd13cc3677214b3cfd9a30517b5f0bc
Victor Velev <velev.victor@yahoo.com>
2018-06-10 18:49:33 +0300

    module match_predictor is working

--- site/server/__init__.py
+++ site/server/__init__.py
@@ -1,7 +1,9 @@
 from app import *
 import routes
-from match_predictor import predict_proba
+from match_predictor.main import predict_proba
 
 if __name__ == "__main__":
-    print(predict_proba("Germany", "Brazil"))
+    print("-"*30)
+    print(predict_proba("Germany", [], "Brazil", []))
+    print("-"*30)
     app.run(debug=True)
--- site/server/match_predictor/mean_stats.py
+++ site/server/match_predictor/mean_stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 __all__ = [
     "get_average_goals",
