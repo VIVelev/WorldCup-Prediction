694052c991cf02f93f3b5c100c4f988d757359c0
kristyanYochev <kris.yochev@gmail.com>
2018-06-11 20:06:35 +0300

    Fixed percentage bug

--- site/server/routes.py
+++ site/server/routes.py
@@ -11,6 +11,11 @@ with open("matches.b", "rb") as f:
 
 @app.route('/')
 def index():
+    for match in matches:
+        match.prob_home = int(match.prob_home * 100)
+        match.prob_away = int(match.prob_away * 100)
+        match.prob_draw = int(match.prob_draw * 100)
+        
     return render_template(
         "index.html", 
         matches=matches,
